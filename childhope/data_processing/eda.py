1c1a5f1cc2ec9381857315f603152b206dd9678d
Koosha <kooshi.ml@gmail.com>
2024-11-02 15:38:42 +0000

    EDAs

--- childhope/data_processing/eda.py
+++ childhope/data_processing/eda.py
@@ -10,10 +10,17 @@ if __name__=="__main__":
    
     path = os.path.join(os.path.dirname(__file__), "../../data/anonimized_vitals.csv")
     logger.info(f"Reading data from {path}")
-    data = pd.read_csv(os.path.join(os.path.dirname(__file__), "../../data/anonimized_vitals.csv"))
+    data = pd.read_csv(os.path.join(os.path.dirname(__file__), "../../data/anonimized_vitals.csv"), index_col=0)
     
     # Generate the profile report
-    logger.info("Generating EDA report")
-    profile = ProfileReport(data, title="ChildHope EDA")
-    profile.to_file(os.path.join(os.path.dirname(__file__), "eda_report.html"))
-    logger.info("EDA report generated successfully and saved to eda_report.html")
+    logger.info("Generating EDA reports")
+    
+    profile = ProfileReport(data, title="General EDA")
+    profile.to_file(os.path.join(os.path.dirname(__file__), "general_eda_report.html"))
+    
+    data = data[data['datetime'].notnull()]
+    data['datetime'] = pd.to_datetime(data['datetime'])
+    profile_ts = ProfileReport(data, title="Time Series EDA", tsmode=True, sortby='datetime')
+    profile_ts.to_file(os.path.join(os.path.dirname(__file__), "ts_eda_report.html"))
+    
+    logger.info("EDA reporst generated successfully and saved to disk.")
